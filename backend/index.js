98dc99cd469f9381c017320e3d7c757fa8e472d9
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-09-14 16:39:38 +0530

    added cache object

--- backend/index.js
+++ backend/index.js
@@ -8,47 +8,61 @@ const cloudinary = require('cloudinary').v2;
 const path = require('path');
 const cors = require('cors')
 
-const dirname=path.resolve();
+const dirname = path.resolve();
 const PORT = 8080;
-require('dotenv').config() 
+require('dotenv').config()
 
 app.use(express.json())
-app.use(express.static(path.join(dirname,"/client/dist")))
+app.use(express.static(path.join(dirname, "/client/dist")))
 app.use(cors())
 
+let cache = {
+  audios: null, // Cache initially empty or null
+};
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-app.get('/',(req,res) => {
-    res.send('hello world')
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
 
 app.get('/audios', async (req, res) => {
-    try {
-        const x = await Audio.find()
-        res.send({x})
-        
-    } catch (error) {
-        res.send({msg: 'something went wrong'})
+  try {
+
+    // Check if data is in cache
+    if (cache.audios) {
+      return res.send({ x: cache.audios });
     }
 
-}) 
+    // Fetch data from database
+    const audios = await Audio.find()
+
+    // Store data in cache
+    cache.audios = audios;
+
+    res.send({ x: audios })
+
+  } catch (error) {
+    res.send({ msg: 'something went wrong' })
+  }
+
+})
 
 
 const upload = multer({ dest: 'uploads/' });
 
 app.post('/upload', upload.single('audio'), async (req, res) => {
 
-  
-  
+
+
   try {
     console.log('body', req.body)
     console.log("file", req.file)
-    const path = req.file.path; 
+    const path = req.file.path;
     console.log(req.file.path)
     const result = await cloudinary.uploader.upload(path, {
       resource_type: 'video',  // Use 'video' for audio files in Cloudinary
@@ -69,14 +83,13 @@ app.post('/upload', upload.single('audio'), async (req, res) => {
   }
 });
 
-app.get("*",(req,res)=>
-    {
-        res.sendFile(path.join(dirname,"client","dist","index.html"))
-    })
+app.get("*", (req, res) => {
+  res.sendFile(path.join(dirname, "client", "dist", "index.html"))
+})
 
 app.listen(PORT, () => {
-    
-    connection(process.env.MONGO_URI)
-    console.log(`server is running on port ${PORT}`)
+
+  connection(process.env.MONGO_URI)
+  console.log(`server is running on port ${PORT}`)
 })
 
